d8a2b4230107f6699f606ca981d68fabf8c44301
Zitschaey <cj.vance@outlook.de>
2023-08-19 23:59:05 +0200

    init commit

---
+++ consumer-isbn/src/test/java/de/pact/consumer/consumerisbn/ConsumerIsbnApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pact.consumer.consumerisbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerIsbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
