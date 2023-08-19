c24a523b46a6bdf667f3a9b5e359f60a815bc8ef
Zitschaey <cj.vance@outlook.de>
2023-08-20 00:05:45 +0200

    Create Basic Structure

---
+++ consumer-isbn/src/main/java/de/pact/consumer/controller/BookController.java
@@ -0,0 +1,5 @@
+package de.pact.consumer.controller;
+
+public class BookController {
+
+}
---
+++ consumer-isbn/src/main/java/de/pact/consumer/pojo/Book.java
@@ -0,0 +1,50 @@
+package de.pact.consumer.pojo;
+
+public class Book {
+
+	private String isbn;
+	private String author;
+	private String title;
+	private long id;
+
+	public Book(String isbn, String author, String title, long id) {
+		super();
+		this.isbn = isbn;
+		this.author = author;
+		this.title = title;
+		this.id = id;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+}
